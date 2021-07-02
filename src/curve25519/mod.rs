af34895781141caf281957dcb9430229e7659a05
Lúcás Meier <cronokirby@gmail.com>
2021-07-02 18:27:54 +0200

    Add failing test for key generation

--- src/curve25519/mod.rs
+++ src/curve25519/mod.rs
@@ -36,3 +36,26 @@ pub fn gen_keypair<R: RngCore + CryptoRng>(rng: &mut R) -> (PublicKey, PrivateKe
     rng.fill_bytes(&mut private.bytes);
     (private.derive_public_key(), private)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_key_derivation_examples() {
+        let mut private = PrivateKey {
+            bytes: [0; 32]
+        };
+        hex::decode_to_slice(
+            "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60",
+            &mut private.bytes
+        ).unwrap();
+        let mut expected = [0; 32];
+        hex::decode_to_slice(
+            "d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a",
+            &mut expected
+        ).unwrap();
+        let public = private.derive_public_key();
+        assert_eq!(public.bytes, expected);
+    }
+}
