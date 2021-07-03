c25d451cabbfae010533de92392035a237ad9588
Lúcás Meier <cronokirby@gmail.com>
2021-07-03 20:38:00 +0200

    Add addition for scalars

--- src/curve25519/scalar.rs
+++ src/curve25519/scalar.rs
@@ -1,6 +1,20 @@
-use std::convert::TryInto;
+use std::{
+    convert::TryInto,
+    ops::{Add, AddAssign},
+};
+
+use subtle::{ConditionallySelectable, ConstantTimeEq};
 
 use super::arithmetic::U256;
+
+const L: U256 = U256 {
+    limbs: [
+        0x5812631a5cf5d3ed,
+        0x14def9dea2f79cd6,
+        0x0000000000000000,
+        0x1000000000000000,
+    ],
+};
 
 /// Represents a scalar in Z/(L) the order of our curve group.
 ///
@@ -29,5 +43,114 @@ impl Scalar {
             value.limbs[i] = u64::from_le_bytes(chunk.try_into().unwrap());
         }
         Scalar { value }
+    }
+
+    pub fn reduce_after_addition(&mut self) {
+        let mut l_removed = *self;
+        let borrow = l_removed.value.sub_with_borrow(L);
+        self.conditional_assign(&l_removed, borrow.ct_eq(&0));
+    }
+}
+
+impl From<u64> for Scalar {
+    fn from(x: u64) -> Self {
+        Scalar {
+            value: U256::from(x),
+        }
+    }
+}
+
+impl ConditionallySelectable for Scalar {
+    fn conditional_select(a: &Self, b: &Self, choice: subtle::Choice) -> Self {
+        Scalar {
+            value: U256::conditional_select(&a.value, &b.value, choice),
+        }
+    }
+}
+
+impl AddAssign for Scalar {
+    fn add_assign(&mut self, other: Self) {
+        self.value += other.value;
+        self.reduce_after_addition();
+    }
+}
+
+impl Add for Scalar {
+    type Output = Self;
+
+    fn add(mut self, other: Self) -> Self::Output {
+        self += other;
+        self
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::curve25519::scalar::L;
+
+    use super::super::arithmetic::U256;
+
+    use super::Scalar;
+    use proptest::prelude::*;
+
+    prop_compose! {
+        fn arb_scalar()(
+            z0 in any::<u64>(),
+            z1 in any::<u64>(),
+            z2 in any::<u64>(),
+            z3 in 0..0xFFFFFFFFFFFFFFFu64) -> Scalar {
+            Scalar {
+                value: U256 { limbs: [z0, z1, z2, z3] }
+            }
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn test_addition_commutative(a in arb_scalar(), b in arb_scalar()) {
+            assert_eq!(a + b, b + a);
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn test_addition_associative(a in arb_scalar(), b in arb_scalar(), c in arb_scalar()) {
+            assert_eq!(a + (b + c), (a + b) + c);
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn test_add_zero_identity(a in arb_scalar()) {
+            let zero = Scalar::from(0);
+            assert_eq!(a + zero, a);
+            assert_eq!(zero + a, a);
+        }
+    }
+
+    #[test]
+    fn test_addition_examples() {
+        let z1 = Scalar {
+            value: U256 {
+                limbs: [1, 1, 1, 1],
+            },
+        };
+        let z2 = Scalar {
+            value: U256 {
+                limbs: [2, 2, 2, 2],
+            },
+        };
+        let z3 = Scalar {
+            value: U256 {
+                limbs: [3, 3, 3, 3],
+            },
+        };
+        assert_eq!(z3, z1 + z2);
+
+        let l_minus_1 = Scalar {
+            value: L - U256::from(1)
+        };
+        assert_eq!(l_minus_1 + Scalar::from(1), Scalar::from(0));
+        assert_eq!(l_minus_1 + Scalar::from(20), Scalar::from(19));
     }
 }
